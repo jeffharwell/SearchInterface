a1fb48c678c0dc31ebdeebef9d80505afc05aeb6
Jeff Harwell <jharwell@fuller.edu>
2015-11-28 00:01:44 +0000

    Initial commit, html5 mode has been enabled but not tested

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name searchInterfaceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the searchInterfaceApp
+ */
+angular.module('searchInterfaceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
