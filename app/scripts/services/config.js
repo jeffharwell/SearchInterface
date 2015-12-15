99060736f1c319f0410e1bbabf390bdc624cd2fe
Jeff Harwell <jharwell@fuller.edu>
2015-12-15 14:54:07 +0000

    Fixed a bad double quote that was causing the build to hang

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -2,6 +2,6 @@
 
 angular.module('services.config', [])
   .constant('configuration', {
-    environment: 'development',
-    backendurl: 'http://triple2.jeffharwell.com/SearchInterface'
+    environment: 'production',
+    backendurl: 'https://triple2.jeffharwell.com/apiservice/SearchInterface'
   });
